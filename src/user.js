fdfdcc5393683d2a74918081119493c7ff234eef
Shen Liu <tuh18904@temple.edu>
2023-10-02 17:16:23 -0400

    jsdoc

---
+++ src/user.js
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together.
+ * @param {number} a The first number.
+ * @param {number} b The second number.
+ * @return {number} The sum of a and b.
+ */
+function add(a, b) {
+    return a + b;
+}
