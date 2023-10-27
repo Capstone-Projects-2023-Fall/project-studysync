b56c336ba8cc3d61d36bdeb7ab302f4ca7f76289
Shen Liu <tuh18904@temple.edu>
2023-10-26 21:40:04 -0400

    now can update set name

--- src/js/react/flashcardCom.js
+++ src/js/react/flashcardCom.js
@@ -23,142 +23,182 @@ const FlashcardComponent = () => {
   const [flashcards, setFlashcards] = useState({});
   const [open, setOpen] = useState(false);
   const [newEntry, setNewEntry] = useState('');
-  const [dialogType, setDialogType] = useState('subject'); // can be 'subject' or 'topic'
+  const [dialogType, setDialogType] = useState('subject');
+  const [editedName, setEditedName] = useState('');
+  const [currentlyEditingTopic, setCurrentlyEditingTopic] = useState(null);
 
   const navigate = useNavigate();
 
-const handleFlashcardClick = async (topicName) => {
+  const handleFlashcardClick = async (topicName) => {
     try {
-        const setId = await FlashcardRepo.getSetIdByTopicName(topicName);
-        if (setId) {
-            navigate(`/flashcard-ui/${setId}`);
-        } else {
-            console.error("Unable to fetch set ID for topic:", topicName);
-        }
+      const setId = await FlashcardRepo.getSetIdByTopicName(topicName);
+      if (setId) {
+        navigate(`/flashcard-ui/${setId}`);
+      } else {
+        console.error("Unable to fetch set ID for topic:", topicName);
+      }
     } catch (error) {
-        console.error("Error in handleFlashcardClick:", error);
+      console.error("Error in handleFlashcardClick:", error);
     }
-};
+  };
 
 
 
 
   useEffect(() => {
     async function fetchData() {
-        try {
-            const uid = FlashcardRepo.getCurrentUid();
-            console.log("User ID:", uid); 
+      try {
+        const uid = FlashcardRepo.getCurrentUid();
+        console.log("User ID:", uid);
 
-            if (uid) {
-                const userSubjects = await FlashcardRepo.getUserSubjects(uid);
-                console.log("Fetched user subjects:", userSubjects); 
-                
-                setSubjects(userSubjects);
-                
-                const ownedFlashcardSetIds = await FlashcardRepo.getUserFlashcardSets(uid);
-                console.log("Fetched flashcard set IDs:", ownedFlashcardSetIds);  
-                
-                const ownedFlashcards = [];
-                for (let setId of ownedFlashcardSetIds) {
-                    const flashcardSet = await FlashcardRepo.getFlashcardSetById(setId);
-                    console.log("Fetched flashcard set:", flashcardSet);  
-                    ownedFlashcards.push(flashcardSet);
-                }
+        if (uid) {
+          const userSubjects = await FlashcardRepo.getUserSubjects(uid);
+          console.log("Fetched user subjects:", userSubjects);
 
-                const flashcardData = {};
-                for (let flashcardSet of ownedFlashcards) {
-                    const { name, subject } = flashcardSet;
-                    if (!flashcardData[subject]) {
-                        flashcardData[subject] = [];
-                    }
-                    flashcardData[subject].push(name);
-                }
-                console.log("Constructed flashcard data:", flashcardData);
-                setFlashcards(flashcardData);
+          setSubjects(userSubjects);
+
+          const ownedFlashcardSetIds = await FlashcardRepo.getUserFlashcardSets(uid);
+          console.log("Fetched flashcard set IDs:", ownedFlashcardSetIds);
+
+          const ownedFlashcards = [];
+          for (let setId of ownedFlashcardSetIds) {
+            const flashcardSet = await FlashcardRepo.getFlashcardSetById(setId);
+            console.log("Fetched flashcard set:", flashcardSet);
+            ownedFlashcards.push(flashcardSet);
+          }
+
+          const flashcardData = {};
+          for (let flashcardSet of ownedFlashcards) {
+            const { name, subject } = flashcardSet;
+            if (!flashcardData[subject]) {
+              flashcardData[subject] = [];
             }
-        } catch (error) {
-            console.error("Error fetching user data:", error);
+            flashcardData[subject].push(name);
+          }
+          console.log("Constructed flashcard data:", flashcardData);
+          setFlashcards(flashcardData);
         }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
     }
     fetchData();
-}, []);
+  }, []);
 
-const handleAddTopic = async () => {
+  const handleAddTopic = async () => {
     try {
-        const uid = FlashcardRepo.getCurrentUid();
-        if (uid) {
-            const flashcardSetId = await FlashcardRepo.createFlashcardSet({    
-                name: newEntry,        
-                subject: selectedSubject
-            });
-            console.log('selectedSubject:', selectedSubject);
+      const uid = FlashcardRepo.getCurrentUid();
+      if (uid) {
+        const flashcardSetId = await FlashcardRepo.createFlashcardSet({
+          name: newEntry,
+          subject: selectedSubject
+        });
+        console.log('selectedSubject:', selectedSubject);
 
-            await FlashcardRepo.addOwnedFlashcardSetToUser(uid, flashcardSetId);
+        await FlashcardRepo.addOwnedFlashcardSetToUser(uid, flashcardSetId);
 
-            // Update the local state
-            const currentFlashcards = flashcards[selectedSubject] || [];
-            setFlashcards(prev => ({
-                ...prev,
-                [selectedSubject]: [...currentFlashcards, newEntry]
-            }));
-        }
-    } catch (error) {
-        console.error("Error adding new topic:", error);
-    }
-};
-
-const handleDelete = async (topicName) => {
-    try {
-        const uid = FlashcardRepo.getCurrentUid();
-
-        
-        const setId = await FlashcardRepo.getSetIdByTopicName(topicName);
-
-        
-        await FlashcardRepo.removeSetIdFromUser(uid, setId);
-
-        
-        await FlashcardRepo.removeUidFromSharedWith(setId, uid);
-
-        const updatedFlashcards = flashcards[selectedSubject].filter(t => t !== topicName);
+        // Update the local state
+        const currentFlashcards = flashcards[selectedSubject] || [];
         setFlashcards(prev => ({
-            ...prev,
-            [selectedSubject]: updatedFlashcards
+          ...prev,
+          [selectedSubject]: [...currentFlashcards, newEntry]
         }));
+      }
     } catch (error) {
-        console.error("Error deleting flashcard set:", error);
+      console.error("Error adding new topic:", error);
     }
-};
+  };
 
-const handleAdd = async () => {
-  const trimmedEntry = newEntry.trim();
+  const handleDelete = async (topicName) => {
+    try {
+      const uid = FlashcardRepo.getCurrentUid();
 
-  if (trimmedEntry) {
+
+      const setId = await FlashcardRepo.getSetIdByTopicName(topicName);
+
+
+      await FlashcardRepo.removeSetIdFromUser(uid, setId);
+
+
+      await FlashcardRepo.removeUidFromSharedWith(setId, uid);
+
+      const updatedFlashcards = flashcards[selectedSubject].filter(t => t !== topicName);
+      setFlashcards(prev => ({
+        ...prev,
+        [selectedSubject]: updatedFlashcards
+      }));
+    } catch (error) {
+      console.error("Error deleting flashcard set:", error);
+    }
+  };
+
+  const handleAdd = async () => {
+    const trimmedEntry = newEntry.trim();
+
+    if (trimmedEntry) {
       if (dialogType === 'subject') {
-          if (!subjects.includes(trimmedEntry)) {
-              // Update the local state
-              setSubjects(prev => [...prev, trimmedEntry]);
-              setFlashcards(prev => ({ ...prev, [trimmedEntry]: [] }));
+        if (!subjects.includes(trimmedEntry)) {
+          // Update the local state
+          setSubjects(prev => [...prev, trimmedEntry]);
+          setFlashcards(prev => ({ ...prev, [trimmedEntry]: [] }));
 
-              // Update the Firebase database
-              const uid = FlashcardRepo.getCurrentUid();
-              if (uid) {
-                  try {
-                      await FlashcardRepo.addUserSubject(uid, trimmedEntry);
-                  } catch (error) {
-                      console.error("Error adding subject to Firebase:", error);
-                  }
-              }
-          }
-      
-        } else if (dialogType === 'topic' && selectedSubject) {
-            
-            handleAddTopic();
+          // Update the Firebase database
+          const uid = FlashcardRepo.getCurrentUid();
+          if (uid) {
+            try {
+              await FlashcardRepo.addUserSubject(uid, trimmedEntry);
+            } catch (error) {
+              console.error("Error adding subject to Firebase:", error);
             }
+          }
         }
-        setNewEntry('');
-        setOpen(false);
+
+      } else if (dialogType === 'topic' && selectedSubject) {
+
+        handleAddTopic();
+      }
     }
+    setNewEntry('');
+    setOpen(false);
+  };
+
+  const handleEdit = async () => {
+    console.log('handleEdit called');  // 确定函数被调用
+
+    if (editedName.trim() === "" || !currentlyEditingTopic) {
+      console.log('Empty edited name or no topic being edited, exiting');  // 检查是否有有效的编辑内容
+      return;
+    }
+
+    try {
+
+      const setId = await FlashcardRepo.getSetIdByTopicName(currentlyEditingTopic);
+      console.log('Set ID by topic name:', setId);
+
+      // Update the Firebase database
+      await FlashcardRepo.updateFlashcardSetName(setId, editedName.trim());
+
+      // Update the local state
+      const updatedFlashcards = flashcards[selectedSubject].map(t => t === currentlyEditingTopic ? editedName.trim() : t);
+      console.log('Updated flashcards:', updatedFlashcards);
+
+      setFlashcards(prev => ({
+        ...prev,
+        [selectedSubject]: updatedFlashcards
+      }));
+
+      setCurrentlyEditingTopic(null);
+      setEditedName('');
+
+
+    } catch (error) {
+      console.error("Error editing flashcard set name:", error);
+    }
+  };
+
+
+
+
 
 
   return (
@@ -172,23 +212,23 @@ const handleAdd = async () => {
               button
               key={subject}
               onClick={() => {
-                console.log('Selected subject:', subject);  
+                console.log('Selected subject:', subject);
                 setSelectedSubject(subject);
-            }}
+              }}
               style={{ padding: '15px 10px' }}
             >
               <ListItemText primary={subject} />
             </ListItem>
           ))}
         </List>
-        <Button 
-          variant="contained" 
-          color="primary" 
-          style={{ 
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
             margin: 'auto 10px 10px 10px',
             backgroundColor: astyle.colors.primary,
             borderRadius: astyle.borderRadius
-          }} 
+          }}
           onClick={() => {
             setOpen(true);
             setDialogType('subject');
@@ -223,9 +263,34 @@ const handleAdd = async () => {
         />
         {flashcards[selectedSubject]?.map((topic, index) => (
           <div key={index} style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px' }}>
-            <h3>{topic}</h3>
+            {
+              currentlyEditingTopic === topic ? (
+                <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+                  <TextField
+                    variant="outlined"
+                    size="small"
+                    value={editedName}
+                    onChange={(e) => setEditedName(e.target.value)}
+                  />
+                  <Button variant="contained" color="primary" onClick={handleEdit}>Save</Button>
+                </div>
+              ) : (
+                <h3>{topic}</h3>
+              )
+            }
             <div style={{ display: 'flex', gap: '10px' }}>
-              <Button variant="outlined">Edit</Button>
+              <Button variant="outlined" onClick={() => {
+
+                if (currentlyEditingTopic) {
+
+                  setCurrentlyEditingTopic(null);
+                  setEditedName('');
+                } else {
+
+                  setCurrentlyEditingTopic(topic);
+                  setEditedName(topic);
+                }
+              }}>Edit</Button>
               <Button variant="outlined" onClick={() => handleFlashcardClick(topic)}>Flashcard</Button>
               <Button variant="outlined">Quiz</Button>
               <Button variant="outlined">AITutor</Button>
@@ -262,7 +327,7 @@ const handleAdd = async () => {
         </DialogActions>
       </Dialog>
     </div>
-  
+
   );
 };
 
