86a472bf09ec4dc1adceee011502d20218f83351
mingjiew522 <tuo17432@temple.edu>
2023-10-27 00:26:43 -0400

    leaderboard buttom added in the quiz page

--- src/js/react/MainQuizPage.js
+++ src/js/react/MainQuizPage.js
@@ -2,26 +2,26 @@ import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Button, List, ListItem, Paper } from '@mui/material';
 
 function MainQuizPage() {
+
     const [selectedSubject, setSelectedSubject] = useState(null);
     const subjects = ['Math', 'History', 'Science', 'English'];
-    const quizzes = {
-        Math: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4'],
-        History: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4'],
-        Science: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4'], 
-        English: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4'],
-   
-    };
-    
+
     return (
         <div>
             {/* Navigation Bar */}
             <AppBar position="static">
-                <Toolbar style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Toolbar style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant="h4" component="h2" style={{ margin: 0 }}>
                         Quiz
                     </Typography>
+                    <Button 
+                        variant="contained"
+                        // for fitutre ;learderboard page implement navigation
+                    >leaderboard
+                    </Button>
                 </Toolbar>
             </AppBar>
+
             <div style={{ display: 'flex', marginTop: '20px' }}>
                 {/* Sidebar */}
                 <Paper elevation={3} style={{ width: '20%', maxHeight: '100vh', overflow: 'auto' }}>
@@ -33,27 +33,18 @@ function MainQuizPage() {
                         ))}
                     </List>
                 </Paper>
-                
+
                 {/* Main Content */}
-                    <div style={{ flex: 1, padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                     {selectedSubject && (
-                                <>
-                             <Typography variant="h5" component="h3" style={{ margin: '20px 0' }}>
-                                  Quizzes for {selectedSubject}
-                             </Typography>
-                              <List>
-                                  {quizzes[selectedSubject].map((quiz) => (
-                                        <ListItem button key={quiz}>
-                                            {quiz}
-                                        </ListItem>
-                                  ))}
-                               </List>
-                            </>
-                      )}
-                    </div>
-             </div>
+                <div style={{ flex: 1, padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    {selectedSubject && (
+                        <>
+                            {/* other content related to selectedSubject*/}
+                        </>
+                    )}
+                </div>
             </div>
-       );
+        </div>
+    );
 }
 
 export default MainQuizPage;
