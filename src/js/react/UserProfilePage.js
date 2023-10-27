4914e2b42570074e36f8c26b6f1e154071d43c2d
atoaidoo <atoaidook@gmail.com>
2023-10-27 16:58:23 -0400

    Fix merge conflicts

--- src/js/react/UserProfilePage.js
+++ src/js/react/UserProfilePage.js
@@ -138,12 +138,12 @@ const  UserProfile = ()=> {
                         <div class="tab-content profile-tab" id="myTabContent">
                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                                         <div class="row">
-                                            {/* <div class="col-md-6">
+                                            <div class="col-md-6">
                                                 <label>User Id</label>
                                             </div>
                                             <div class="col-md-6">
-                                                <p>{profile.name}</p>
-                                            </div> */}
+                                                <p>{UserId}</p>
+                                            </div>
                                         </div>
                                         <div class="row">
                                             <div class="col-md-6">
