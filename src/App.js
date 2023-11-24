44289ee98879f5cdc248fd6675cea2c5cb6ddfa3
Harris Kwong <harriskwong120898@gmail.com>
2023-11-24 13:38:11 -0500

    Cleaned up code.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import FlashcardApp from './js/react/flashcardUICom';
 import PasswordReset from './js/react/PasswordReset';
 import QuizList from './js/react/quizCom';
 import UserProfile from './js/react/UserProfilePage';
-import EditUserProfile from './js/react/EditUserProfile';
 import FriendsPage from './js/react/FriendsPage';
 import MainQuizPage from './js/react/MainQuizPage';
 import Quiz from './js/react/Quiz.js';
@@ -58,7 +57,6 @@ function App() {
         <Route path='/passwordreset' element={<PasswordReset />} />
         <Route path='/signup' element={<SignUpForm />} />
         <Route path='/profile/:UserId' element={<UserProfile />} />
-        <Route path='/profile/:data/edit' element={<EditUserProfile />} />
         <Route path='/quizmain' element={<MainQuizPage />} />
         <Route path='/socials/:UserId' element={<FriendsPage/>}/>
         <Route path='/notifications/:UserId' element={<Notification/>}/>
