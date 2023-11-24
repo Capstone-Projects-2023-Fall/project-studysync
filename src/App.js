c0a2de010bb1c6b358b8757c8a3a86c9b671eeb7
Harris Kwong <harriskwong120898@gmail.com>
2023-11-24 15:53:35 -0500

    fixed code

--- src/App.js
+++ src/App.js
@@ -26,9 +26,15 @@ function App() {
     { label: 'Sign Up', link: '/signup' },
   ];
 
+
   const navbarItemsLoggedIn = [
     { label: 'Dashboard', link: '/dashboard' },
     { label: 'Flashcard', link: '/flashcard' },
+<<<<<<< HEAD
+=======
+    { label: 'Quiz', link: '/quizmain' },
+   
+>>>>>>> parent of 81268df (cleaned code)
     { label: 'Profile', link: `/profile/${user && user.uid}` },
     {label: 'Socials',link:`/socials/${user && user.uid}`},
     {label: 'Notifications',link:`/notifications/${user && user.uid}`}
