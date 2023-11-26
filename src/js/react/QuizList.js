ba9f5b29196dacfae6fe2b4faec3582ec6502d61
Leapheng Keam <tuk74678@gmail.com>
2023-11-25 22:47:54 -0500

    add a nutton for user to clicks Rename, or Delete

--- src/js/react/QuizList.js
+++ src/js/react/QuizList.js
@@ -11,11 +11,21 @@ import ListItemText from '@mui/material/ListItemText';
 import QuizIcon from '@mui/icons-material/Quiz';
 import FlashcardRepo from '../repositories/FlashcardRepo';
 import { useParams, useNavigate } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
 
 function QuizList() {
   const [state, setState] = React.useState({
     left: false,
   });
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const [currentQuizMenu, setCurrentQuizMenu] = React.useState(null);
+  const [quizMenu, setQuizMenu] = React.useState(null);
 
   const [quizList, setQuizList] = useState([]); // store the quiz title that retreived from database
   const { setId, quizId } = useParams();  // retrieve the flashcard set in order to go to a certain quiz
@@ -29,12 +39,12 @@ function QuizList() {
         console.log("Fetching Quiz Title:", quizData);
         setQuizList(quizData);
 
-        // If quizId is available, set it as the current open quiz
+        // if quizId is available, set it as the current open quiz
         if (quizId) {
           setCurrentQuiz(quizId);
           console.log("Current Quiz ID: ", quizId);
         } else if (quizData.length > 0) {
-          // If no specific quizId is provided, navigate to the first quiz in the list by default
+          // if no specific quizId is provided, navigate to the first quiz in the list by default
           const firstQuizId = await FlashcardRepo.getQuizTitleId(quizData[0]);
           const firstSetId = await FlashcardRepo.getSetIdByQuizId(firstQuizId);
           navigate(`/quizFlash/${firstSetId}/quiz/${firstQuizId}`);
@@ -50,7 +60,7 @@ function QuizList() {
   
   }, [setId, quizId, navigate]);
 
-  //navigate to quiz page by passing flashcardSet ID as parameter
+  // navigate to quiz page by passing flashcardSet ID as parameter
   const handleQuizTitleClick = async (quizName) => {
       try {
         const quizId = await FlashcardRepo.getQuizTitleId(quizName);
@@ -70,7 +80,7 @@ function QuizList() {
   
   };
 
-  //navigate to the defualt quiz page by passing flashcardSet ID as parameter
+  // navigate to the defualt quiz page by passing flashcardSet ID as parameter
   const handleDefaultQuizClick = async () => {
     try {
       if (setId) {
@@ -95,6 +105,32 @@ function QuizList() {
     setState({ ...state, [anchor]: open });
   };
 
+  // handle the MoreVertIcon when clicked
+  const handleClick = (event, menuType) => {
+    // stop the event propagation to prevent the drawer from closing
+    event.stopPropagation();
+  
+    setAnchorEl(event.currentTarget);
+  
+    // set the appropriate menu state based on the type
+    if (menuType === 'currentQuiz') {
+      setCurrentQuizMenu(event.currentTarget);
+    } else if (menuType === 'quiz') {
+      setQuizMenu(event.currentTarget);
+    }
+  };
+
+  const handleClose = (menuType) => {
+    setAnchorEl(null);
+
+    // close the appropriate menu based on the type
+    if (menuType === 'currentQuiz') {
+      setCurrentQuizMenu(null);
+    } else if (menuType === 'quiz') {
+      setQuizMenu(null);
+    }
+  };
+
   const list = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
@@ -113,6 +149,27 @@ function QuizList() {
                 <QuizIcon/>
               </ListItemIcon>
               <ListItemText primary={`Default Quiz`} />
+              <IconButton
+                  id="current-quiz-button"
+                  aria-controls={currentQuizMenu ? 'current-quiz-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={Boolean(currentQuizMenu)}
+                  onClick={(event) => handleClick(event, 'currentQuiz')}
+              >
+                <MoreVertIcon />
+              </IconButton>
+                <Menu
+                  id="current-quiz-menu"
+                  anchorEl={currentQuizMenu}
+                  open={Boolean(currentQuizMenu)}
+                  onClose={() => handleClose('currentQuiz')}
+                  MenuListProps={{
+                    'aria-labelledby': 'current-quiz-button',
+                  }}
+                >
+                  <MenuItem onClick={() => handleClose('currentQuiz')}>Profile</MenuItem>
+                  <MenuItem onClick={() => handleClose('currentQuiz')}>My account</MenuItem>
+                </Menu>
             </ListItemButton>
           </ListItem>
         )}
@@ -123,6 +180,28 @@ function QuizList() {
                 <QuizIcon/>
               </ListItemIcon>
               <ListItemText primary={quizTitle} />
+              <IconButton
+                  id={`quiz-button-${quizTitle}`}
+                  aria-controls={quizMenu ? `quiz-menu-${quizTitle}` : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={Boolean(quizMenu)}
+                  onClick={(event) => handleClick(event, 'quiz')}
+                >
+                <MoreVertIcon />
+              </IconButton>
+                <Menu
+                  id={`quiz-menu-${quizTitle}`}
+                  anchorEl={quizMenu}
+                  open={Boolean(quizMenu)}
+                  onClose={() => handleClose('quiz')}
+                  MenuListProps={{
+                    'aria-labelledby': `quiz-button-${quizTitle}`,
+                  }}
+                >
+                  <MenuItem onClick={() => handleClose('quiz')}>Rename</MenuItem>
+                  <MenuItem onClick={() => handleClose('quiz')}>Delete</MenuItem>
+                  <MenuItem onClick={() => handleClose('quiz')}>Share</MenuItem>
+                </Menu>
             </ListItemButton>
           </ListItem>
           ))}
