ce5bc3e866bbbba237b42a056b3af1dfe327be5f
Shen Liu <111998266+tuh18904temple@users.noreply.github.com>
2023-11-29 20:36:01 -0500

    Update docusaurus.config.js

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
  * The URL or reference to your projects logo!
  * @type {string}
  */
-const logo = './static/img/docusaurus.png';
+const logo = './static/img/logo.svg';
 
 const main_template_jira_scripts = () => {
   if (process.env.ORG_NAME === 'ApplebaumIan') {
