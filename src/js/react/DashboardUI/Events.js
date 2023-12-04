ab520577229f2c62fd4c14c9458bc8cfca36694a
Harris Kwong <harriskwong120898@gmail.com>
2023-12-03 22:47:32 -0500

    Display default images for users without profile picture on the dashboard

--- src/js/react/DashboardUI/Events.js
+++ src/js/react/DashboardUI/Events.js
@@ -9,13 +9,28 @@ import TableRow from '@mui/material/TableRow';
 import './Events.css';
 
 export default function Events(props) {
-  const {events} = props;
+  let {events} = props;
+  events = events.sort();
+
   const _events = events.slice(0,4);
 
   const convertToDate=(event)=>{
     let new_date = new Date(event.timestamp);
     let dateArr = new_date.toString().split(" ");
-    let display = dateArr[0] + " "+dateArr[1];
+    const inputTime = dateArr[4] + " "+dateArr[5];
+    // Extract hours and minutes from the input time
+    const [hours, minutes] = inputTime.split(":").map(Number);
+
+    // Create a new Date object and set hours and minutes
+    const date = new Date();
+    date.setUTCHours(hours);
+    date.setUTCMinutes(minutes);
+
+    // Convert to Eastern Standard Time (EST)
+    const estTime = date.toLocaleTimeString("en-US", { timeZone: "America/New_York" });
+
+    let display = dateArr[0] + " "+dateArr[1]+ " "+ dateArr[2] + 
+      " "+ dateArr[3] + " " + dateArr[4]+" "+dateArr[5];
     return display;
   }
 
