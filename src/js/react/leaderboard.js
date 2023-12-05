84e34601e919bf7b3d3284067e64736de2c51305
mingjiew522 <tuo17432@temple.edu>
2023-12-05 18:26:27 -0500

    initial desing of leaderboard

--- src/js/react/leaderboard.js
+++ src/js/react/leaderboard.js
@@ -1,70 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { database } from '../../firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import React from 'react';
 
-const QuizHistory = () => {
-  const [quizAttempts, setQuizAttempts] = useState([]);
-  const [totalQuizzesTaken, setTotalQuizzesTaken] = useState(0);
-  const [averageScore, setAverageScore] = useState(0);
+// Sample data
+const usersData = [
+    { id: 1, name: 'Alice', totalQuizzes: 5, averageScore: 80 },
+    { id: 2, name: 'Bob', totalQuizzes: 3, averageScore: 70 },
+    { id: 3, name: 'Charlie', totalQuizzes: 8, averageScore: 90 },
+    // ... add more users as needed
+];
 
-  useEffect(() => {
-    const fetchQuizAttempts = async () => {
-      const quizAttemptsCollectionRef = collection(database, 'score'); // Adjust if your collection name is different
-    
+class Leaderboard extends React.Component {
+    renderLeaderboard(data, title, key) {
+        return (
+            <div>
+                <h2>{title}</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>{key}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(user => (
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{user[key]}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 
-      try {
-        const querySnapshot = await getDocs(q);
-        const attempts = querySnapshot.docs.map(doc => ({
-          ...doc.data(),
-          submitDate: doc.data().submitAt.toDate().toLocaleDateString(), // Assuming 'submitAt' is a Firestore Timestamp
-          id: doc.id,
-        }));
+    render() {
+        // Sort users by totalQuizzes and averageScore respectively
+        const byTotalQuizzes = [...usersData].sort((a, b) => b.totalQuizzes - a.totalQuizzes);
+        const byAverageScore = [...usersData].sort((a, b) => b.averageScore - a.averageScore);
 
-        // Calculate total quizzes taken and average score
-        const totalQuizzes = attempts.length;
-        const totalScore = attempts.reduce((acc, attempt) => acc + attempt.score, 0);
-        const avgScore = totalQuizzes > 0 ? (totalScore / totalQuizzes).toFixed(2) : 0;
+        return (
+            <div>
+                {this.renderLeaderboard(byTotalQuizzes, 'Total Quizzes Leaderboard', 'totalQuizzes')}
+                {this.renderLeaderboard(byAverageScore, 'Average Score Leaderboard', 'averageScore')}
+            </div>
+        );
+    }
+}
 
-        setQuizAttempts(attempts);
-        setTotalQuizzesTaken(totalQuizzes);
-        setAverageScore(avgScore);
-      } catch (error) {
-        console.error('Fetching quiz attempts failed:', error);
-      }
-    };
-
-    fetchQuizAttempts();
-  }, [userId]); // Depend on userId to refetch when it changes
-
-  return (
-    <div>
-      <h2>Leaderboard</h2>
-      <div>
-        <strong>Total Quizzes Taken:</strong> {totalQuizzesTaken}
-      </div>
-      <div>
-        <strong>Average Score:</strong> {averageScore}%
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Quiz Name</th>
-            <th>Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {quizAttempts.map((attempt, index) => (
-            <tr key={index}>
-              <td>{attempt.submitDate}</td>
-              <td>{attempt.quizName}</td>
-              <td>{attempt.score}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default QuizHistory;
+export default Leaderboard;
